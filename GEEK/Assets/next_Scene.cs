5f278bd9f478c4f12c8f35f91cf3bdb7b8bc6da9
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2025-02-10 20:21:11 +0100

    UWU

--- GEEK/Assets/next_Scene.cs
+++ GEEK/Assets/next_Scene.cs
@@ -5,13 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class next_Scene : MonoBehaviour
 {
-    public void PlayGame()
+    public GameObject Menu;
+    public GameObject Options;
+    public void ContinueBtn()
     {
         SceneManager.LoadScene("Main_Menu");
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Lobby");
     }
     public void QuitGame()
     {
         Application.Quit();
     }
-
+    public void OpenOptions()
+    {
+        Menu.SetActive(false);
+        Options.SetActive(true);
+    }
+    public void CloseOptions()
+    {
+        Menu.SetActive(true);
+        Options.SetActive(false);
+    }
 }
