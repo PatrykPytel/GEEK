709b106e401ba14ce167f8739d085c8caa3fddb0
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2025-02-23 00:49:18 +0100

    dsa

---
+++ GEEK/Assets/Next_Page.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Next_Page : MonoBehaviour
+{
+    public GameObject currentpage;
+    public GameObject nextpage;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NextPage()
+    {
+        currentpage.SetActive(false);
+        nextpage.SetActive(true);
+
+    }
+
+}
